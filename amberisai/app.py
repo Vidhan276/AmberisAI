43b85a5233e21a45d19d3e99fca6f97023d7c70b
Vidhan276 <vidhanjain277@gmail.com>
2026-03-01 18:29:37 +0530

    Update app.py

--- amberisai/app.py
+++ amberisai/app.py
@@ -23,5 +23,6 @@ app.register_blueprint(predict_bp)
 app.register_blueprint(auth_bp)
 app.register_blueprint(agent_bp)
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000, threaded=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
